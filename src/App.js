da2aa128e73c8418915815f86daa6dd2fa000d74
rrahul chandrakar <rrahulchandrakar1@gmail.com>
2023-06-16 09:20:46 +0530

    Push all files again

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-
-
 function App() {
   return (
     <>
-      
+      <div className="header">
+        <h1 className="text-4xl font-bold underline">
+          Make my Note
+        </h1>
+      </div>
     </>
   );
 }
